c26f8a46e58706c9bfcba1a099e3193071ddeaf3
way <1320438999@qq.com>
2016-04-23 18:43:28 +0800

    update version to r1.0.01

--- app/src/main/java/com/way/capture/utils/ffmpeg/CpuArchHelper.java
+++ app/src/main/java/com/way/capture/utils/ffmpeg/CpuArchHelper.java
@@ -9,13 +9,14 @@ class CpuArchHelper {
         String[] abis = Build.SUPPORTED_ABIS;
         if (abis == null || abis.length < 1)
             return CpuArch.NONE;
-        String cpuArch = abis[0];
-        if (TextUtils.equals(cpuArch, getx86CpuAbi()))
-            return CpuArch.x86;
-        else if (TextUtils.equals(cpuArch, getArmeabiv7CpuAbi()))
-            return CpuArch.ARMv7;
-        else if (TextUtils.equals(cpuArch, getArmeabiCpuAbi())) // TODO: 16/4/23 I don't konw if it's OK!
-            return CpuArch.ARMv7;
+        for(String cpuArch :abis) {
+            if (TextUtils.equals(cpuArch, getx86CpuAbi()))
+                return CpuArch.x86;
+            else if (TextUtils.equals(cpuArch, getArmeabiv7CpuAbi()))
+                return CpuArch.ARMv7;
+            else if (TextUtils.equals(cpuArch, getArmeabiCpuAbi())) // TODO: 16/4/23 I don't konw if it's OK!
+                return CpuArch.ARMv7;
+        }
         return CpuArch.NONE;
     }
 
